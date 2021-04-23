833ec0975ce2c4d7b4b26e6fd33548668bd9e750
Edward Baker <ebbaker@perse.co.uk>
2021-04-23 09:31:48 +0000

    Resizing

--- Ui.py
+++ Ui.py
@@ -1,7 +1,7 @@
 from Game import Game, GameError
 from abc import ABC, abstractmethod
 from itertools import product
-from tkinter import Button, Tk, Toplevel, Frame, X, StringVar
+from tkinter import Button, Tk, Toplevel, Frame, X, StringVar, Text, Scrollbar, LEFT, RIGHT, Y, END, Grid, N, E, W, S
 
 class Ui(ABC):
 
@@ -29,8 +29,16 @@ class Gui(Ui):
             text='Quit',
             command = self._quit_callback).pack(fill=X)
     
-    
+        console = Text(frame,height=4,width=50)
+        scroll = Scrollbar(frame)
+        scroll.pack(side=RIGHT,fill=Y)
+        console.pack(side=LEFT,fill=Y)
+        
+        scroll.config(command=console.yview)
+        console.config(yscrollcommand=scroll.set)
+        
         self.__root = root
+        self.__console = console
 
         ## BUTTON FUNCTIONS ##
     def _help_callback(self):
@@ -41,7 +49,13 @@ class Gui(Ui):
         game_win = Toplevel(self.__root)
         game_win.title("Game")
         frame = Frame(game_win)
-        frame.grid(row=0,column=0)
+        
+        #Resizing
+        Grid.columnconfigure(game_win,0,weight=1)
+        Grid.rowconfigure(game_win,0,weight=1)        
+        frame.grid(row=0,column=0,sticky=N+S+W+E)
+        
+        
         
         Button(game_win, text="Dismiss", command = game_win.destroy).grid(row=1,column=0)
         
@@ -54,15 +68,31 @@ class Gui(Ui):
             
             cmd = lambda r=row, c=col: self.__play_and_refresh(r,c)
             
-            Button(frame,textvariable=b,command=cmd).grid(row=row,column=col)
+            Button(frame,textvariable=b,command=cmd).grid(row=row,column=col,sticky=N+S+W+E)
             self.__buttons[row][col] = b
             
+        #resizing
+        for i in range(3):
+            Grid.columnconfigure(frame,i,weight=1)
+            Grid.rowconfigure(frame,i,weight=1)        
+            
     def __play_and_refresh(self,row,col):
-        self.__game.play(row+1,col+1)
-        
+        try:
+            self.__game.play(row+1,col+1)
+        except GameError as e:
+            self.__console.insert(END,f"{e}\n")
+            
+            
         for row,col in product(range(3),range(3)):
             text = self.__game.at(row+1,col+1)
             self.__buttons[row][col].set(text)
+            
+        w = self.__game.winner
+        if w != None:
+            if w is Game.DRAW:
+                self.__console.insert(END, "The game was drawn!\n")
+            else:
+                self.__console.insert(END, f"The winner was {w}\n")
         
         
     def _quit_callback(self):
