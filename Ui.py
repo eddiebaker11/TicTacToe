5cb111f7cd5bb60a2b2c16ba907f3524c246dde4
Edward Baker <ebbaker@perse.co.uk>
2021-04-27 11:48:40 +0000

    Added help window

--- Ui.py
+++ Ui.py
@@ -1,7 +1,7 @@
 from Game import Game, GameError
 from abc import ABC, abstractmethod
 from itertools import product
-from tkinter import Button, Tk, Toplevel, Frame, X, StringVar, Text, Scrollbar, LEFT, RIGHT, Y, END, Grid, N, E, W, S
+from tkinter import Button, Tk, Toplevel, Frame, X, StringVar, Text, Scrollbar, LEFT, RIGHT, Y, END, Grid, N, E, W, S, Message
 
 class Ui(ABC):
 
@@ -39,25 +39,36 @@ class Gui(Ui):
         
         self.__root = root
         self.__console = console
+        self.__GameInProgress = False
 
         ## BUTTON FUNCTIONS ##
     def _help_callback(self):
-        pass
+        help_win = Toplevel(self.__root)
+        help_text = "blah blah"
+        Message(help_win, text=help_text).pack(fill=X)
+        Button(help_win,text="Dismiss",command=help_win.destroy).pack(fill=X)
+    
+    def _dismiss_game(self):
+        self.__GameInProgress = False
+        self.__game_win.destroy()
     
     def _play_callback(self):
+        if self.__GameInProgress:
+            return
+        self.__GameInProgress = True
+        self.__Finished = False
         self.__game = Game()
         game_win = Toplevel(self.__root)
         game_win.title("Game")
         frame = Frame(game_win)
+        self.__game_win = game_win
         
         #Resizing
         Grid.columnconfigure(game_win,0,weight=1)
         Grid.rowconfigure(game_win,0,weight=1)        
         frame.grid(row=0,column=0,sticky=N+S+W+E)
         
-        
-        
-        Button(game_win, text="Dismiss", command = game_win.destroy).grid(row=1,column=0)
+        Button(game_win, text="Dismiss", command = self._dismiss_game).grid(row=1,column=0)
         
         # only 1 game at a time
         self.__buttons = [[None]*3 for _ in range(3)]
@@ -77,6 +88,8 @@ class Gui(Ui):
             Grid.rowconfigure(frame,i,weight=1)        
             
     def __play_and_refresh(self,row,col):
+        if self.__Finished:
+            return
         try:
             self.__game.play(row+1,col+1)
         except GameError as e:
@@ -88,7 +101,8 @@ class Gui(Ui):
             self.__buttons[row][col].set(text)
             
         w = self.__game.winner
-        if w != None:
+        if w is not None:
+            self.__Finished = True
             if w is Game.DRAW:
                 self.__console.insert(END, "The game was drawn!\n")
             else:
